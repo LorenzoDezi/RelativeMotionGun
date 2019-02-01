8cfe8bfca940ab9cffb69f106e94b091a4a000ff
Lorenzo Dezi <lorenzo.dezi@studenti.unicam.it>
2019-02-01 19:45:02 +0100

    Dynamics part scene completed.

---
+++ Assets/Scripts/DynamicRelMotGunController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DynamicRelMotGunController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float maxLength = 18f;
+    private bool hasShoot = false;
+    public bool HasShoot {
+        get { return hasShoot; }
+        set { hasShoot = value; }
+    }
+    [SerializeField]
+    private Rigidbody ball;
+    [SerializeField]
+    private float ballUpSpeed = 3f;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = speed * transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(transform.position.z) > maxLength)
+        {
+            speed = -speed;
+            GetComponent<Rigidbody>().velocity = speed * transform.forward;
+        }
+    }
+
+    public void Shoot()
+    {
+        if (ball != null)
+        {
+            ball.velocity = new Vector3(0, 5*ballUpSpeed, GetComponent<Rigidbody>().velocity.z);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RelMotGunTipComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelMotGunTipComponent : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Trigger")) return;
+        var controller = GetComponentInParent<DynamicRelMotGunController>();
+        if (!controller.HasShoot)
+            controller.Shoot();
+        controller.HasShoot = !controller.HasShoot;
+    }
+}
